0c5fc5e85636b883fdc9e38b49e8616e92134756
Vladimir Drizheruk <vdrizheruk@magecore.com>
2015-04-24 19:23:13 +0300

    Added to Letter Attachment Activity

---
+++ src/Mailsystem/Bundle/LetterBundle/Migrations/Schema/v1_2/MailsystemLetterBundle.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mailsystem\Bundle\LetterBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+
+class MailsystemLetterBundle implements Migration, ActivityExtensionAwareInterface
+{
+    /** @var ActivityExtension */
+    protected $activityExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        self::addActivityAssociations($schema, $this->activityExtension);
+    }
+
+    /**
+     * Enables Attachment activity for Letter entity
+     *
+     * @param Schema            $schema
+     * @param ActivityExtension $activityExtension
+     */
+    public static function addActivityAssociations(Schema $schema, ActivityExtension $activityExtension)
+    {
+        $activityExtension->addActivityAssociation($schema, 'oro_attachment', 'mailsystem_letter', true);
+    }
+}
