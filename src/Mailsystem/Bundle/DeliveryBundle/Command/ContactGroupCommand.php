25a88d4997227a9bba33f8891e2a723770dbc63c
Vladimir Drizheruk <vladimir@drizheruk.com.ua>
2015-10-26 16:06:48 +0200

    mailing

--- src/Mailsystem/Bundle/DeliveryBundle/Command/ContactGroupCommand.php
+++ src/Mailsystem/Bundle/DeliveryBundle/Command/ContactGroupCommand.php
@@ -3,6 +3,7 @@
 namespace Mailsystem\Bundle\DeliveryBundle\Command;
 
 use Doctrine\ORM\EntityManager;
+
 use Mailsystem\Bundle\LetterBundle\Entity\Letter;
 use Oro\Bundle\EmailBundle\Model\EmailRecipientsProviderArgs;
 use OroCRM\Bundle\ContactBundle\Entity\Group as ContactGroup;
@@ -27,6 +28,12 @@ class ContactGroupCommand extends CommandAbstract
         return 'Sending Email to Contact Group';
     }
 
+    public function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $this->mailer = $this->getContainer()->get('mailer');
+    }
+
     public function configure()
     {
         parent::configure();
@@ -41,40 +48,57 @@ class ContactGroupCommand extends CommandAbstract
             InputArgument::REQUIRED,
             'Contact Group Id'
         );
+        $this->addArgument(
+            'send_from_email',
+            InputArgument:REQUIRED,
+            'Sender email'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $letterId       = $input->getArgument('letter');
+        $letterId = $input->getArgument('letter');
         $contactGroupId = $input->getArgument('contact-group');
 
         $input->validate();
 
-        $this->em     = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $this->mailer = $this->getContainer()->get('mailer');
         /** @var Letter $letter */
         $letter = $this->em->getRepository('MailsystemLetterBundle:Letter')->find($letterId);
         $output->writeln(sprintf('Letter : %s', $letter->getSubject()));
         /** @var ContactGroup $contactGroup */
         $contactGroup = $this->em->getRepository('OroCRMContactBundle:Group')->find($contactGroupId);
         $output->writeln(sprintf('Contact Group : %s', $contactGroup->getLabel()));
-        $this->sendEmail($letter, $contactGroup);
+        $this->sendEmail($letter, $contactGroup, function ($data) {
+            $output->writeln($data);
+        });
     }
 
-    protected function sendEmail(Letter $letter, ContactGroup $contactGroup)
+    protected function sendEmail(Letter $letter, ContactGroup $contactGroup, $callback = null)
     {
-        $provider = $this->getContainer()->get('orocrm_contact.provider.email_recipients');
-        print_r($contactGroup->getLabel());
-        $args = new EmailRecipientsProviderArgs($contactGroup, null, 2);
-        print_r($provider->getRecipients($args));
+        $stmt = $this->em
+            ->getConnection()
+            ->prepare('select contact_id from  orocrm_contact_to_contact_grp where contact_group_id = :group_id');
+        $stmt->bindValue(':group_id', $contactGroup->getId());
+        $stmt->execute();
+        $contactIds = $stmt->fetchAll();
+        if (!count($contactIds)) {
+            echo sprintf('No Contacts Found in Group %s', $contactGroup->getLabel());
+            exit;
+        }
+        foreach ($contactIds as $contactId) {
+            $contact = $this->em->getRepository('OroCRMContactBundle:Contact')->find($contactId['contact_id']);
+            $email = $contact->getEmail();
 
-        exit;
-        $message = \Swift_Message::newInstance()
-            ->setSubject($letter->getSubject())
-            ->setFrom('vladimir@drizheruk.com.ua', 'Vladimir Drizheruk')
-            ->setTo('vladimir@drizheruk.com.ua')
-            ->setBody($letter->getBody(), 'text/html');
-
-        $this->mailer->send($message);
+            $message = \Swift_Message::newInstance()
+                ->setSubject($letter->getSubject())
+                ->setFrom('vladimir@drizheruk.com.ua', 'Vladimir Drizheruk')
+                ->setTo('vladimir@drizheruk.com.ua')
+                ->setBody($letter->getBody(), 'text/html');
+            if ($this->mailer->send($message)) {
+                echo sprintf("Send to %s", $email) . PHP_EOL . PHP_EOL;
+            }
+            unset($email);
+            unset($message);
+        }
     }
 }
